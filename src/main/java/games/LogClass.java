7fbcbe06678d9254d7b3a144f9554ac4749121c0
wohorogov <wohorogov@mail.ru>
2022-07-25 14:52:40 +0500

    log for Slot on console

---
+++ src/main/java/games/LogClass.java
@@ -0,0 +1,7 @@
+package games;
+
+import org.slf4j.Logger;
+
+public class LogClass {
+    static final Logger log = org.slf4j.LoggerFactory.getLogger(LogClass.class);
+}
--- src/main/java/games/Slot.java
+++ src/main/java/games/Slot.java
@@ -7,25 +7,31 @@ public class Slot {
     public static void main() {
         int firstCounter = 0, secondCounter = 0, thirdCounter = 0, size = 7;
         int balance = 100;
-        System.out.println("У вас " + balance + "$, ставка - 10%");
-
+        //System.out.println("У вас " + balance + "$, ставка - 10%");
+        LogClass.log.info("У вас " + balance + "$, ставка - 10%");
         do {
             firstCounter = (firstCounter + (int) round(random() * 100)) % size;
             secondCounter = (secondCounter + (int) round(random() * 100)) % size;
             thirdCounter = (thirdCounter + (int) round(random() * 100)) % size;
-            System.out.println("Крутим барабаны! Розыгрыш принес следующие результаты:");
-            System.out.println("Первый барабан " + firstCounter + ", второй барабан " +
-                                secondCounter + ", третий барабан " + thirdCounter);
+            LogClass.log.info("Крутим барабаны! Розыгрыш принес следующие результаты:");
+            //System.out.println("Крутим барабаны! Розыгрыш принес следующие результаты:");
+            //System.out.println("Первый барабан " + firstCounter + ", второй барабан " +
+            //                    secondCounter + ", третий барабан " + thirdCounter);
+            LogClass.log.info("Первый барабан " + firstCounter + ", второй барабан " +
+                              secondCounter + ", третий барабан " + thirdCounter);
             if (firstCounter == secondCounter && firstCounter == thirdCounter) {
                 balance += 1000;
-                System.out.println("Победа! Ваш капитал теперь составляет: " + balance);
+                LogClass.log.info("Победа! Ваш капитал теперь составляет: " + balance);
+                //System.out.println("Победа! Ваш капитал теперь составляет: " + balance);
             }
             else {
                 balance -= 10;
-                System.out.println("Проигрыш 10%, Ваш капитал теперь составляет: " + balance);
+                LogClass.log.info("Проигрыш 10%, Ваш капитал теперь составляет: " + balance);
+                //System.out.println("Проигрыш 10%, Ваш капитал теперь составляет: " + balance);
             }
         }
         while (balance > 0);
-        System.out.println("Игра окончена!");
+        LogClass.log.info("Игра окончена!");
+        //System.out.println("Игра окончена!");
     }
 }
